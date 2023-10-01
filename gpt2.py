757d5c990ff6a3202e2cbd319f13d75e37e3ba58
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2023-10-01 02:30:58 -0400

    Update gpt2.py

--- gpt2.py
+++ gpt2.py
@@ -1,8 +1,6 @@
 from summarizer import TransformerSummarizer
-body=""
-with open("19710021280_clean.txt","r") as f:
-    body=f.read()
-print(body)
-GPT2_model = TransformerSummarizer(transformer_type="GPT2",transformer_model_key="gpt2-medium")
-full = ''.join(GPT2_model(body, min_length=60))
-print(full)
+def pdf_summary(body):
+    
+    GPT2_model = TransformerSummarizer(transformer_type="GPT2",transformer_model_key="gpt2-medium")
+    full = ''.join(GPT2_model(body, min_length=100))
+    return full
